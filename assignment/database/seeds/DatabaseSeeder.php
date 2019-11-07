654a0ec4dbc0b6981d31f16f99b08ef320a6760a
Alexander Masud <alexandermasud@gmail.com>
2019-11-07 17:45:42 +0100

    Changed seed file order
    Laravel needs the import of seeds in correct order.

--- assignment/database/seeds/DatabaseSeeder.php
+++ assignment/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call('ProductsTableSeeder');
+        $this->call('StoresTableSeeder');
+        $this->call('ProductStoresTableSeeder');
+        $this->call('ReviewsTableSeeder');
+
+        //$this->call('UserTableSeeder');
     }
 }
