fc7aa4864461f58784e045dacff3b806132ee6b4
ViktorOlsson <vicke.olsson81@gmail.com>
2019-11-11 19:13:56 +0100

    Added auth and created a real env file also changed database file to work on my machine

--- assignment/resources/views/products/index.blade.php
+++ assignment/resources/views/products/index.blade.php
@@ -13,7 +13,7 @@
        
     </head>
     <body>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      {{-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="./">{{config('app.name', 'Laravel')}}</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -31,7 +31,8 @@
             </li>
           </ul>
         </div>
-      </nav>
+      </nav> --}}
+      @include('layouts.app')
         <div class="container">
 
 
